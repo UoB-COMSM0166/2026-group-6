fbaaf3e488940ab9105ad4b51a44f5922d29184c
n-wind-ddd <luruomu8427506@163.com>
2026-02-11 22:19:04 +0000

    Update game-main.js path

--- game/game-project/game-main.js
+++ game/game-project/game-main.js
@@ -16,8 +16,8 @@ let camY = 0;
 let gameStatus = "PLAY";
 
 function preload() {
-   ldtkData = loadJSON('../map/map-area4.ldtk');
-   tilesetImage = loadImage('../resources/images/map_image/test_allgrid_8px.png');
+   ldtkData = loadJSON('map/map-area4.ldtk');
+   tilesetImage = loadImage('resources/images/map_image/test_allgrid_8px.png');
 }
 
 function setup() {
