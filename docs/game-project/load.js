78222cdbc3785a420abd980e3d7490351bde31b2
n-wind-ddd <luruomu8427506@163.com>
2026-02-14 17:39:45 +0000

    platform.js

--- docs/game-project/load.js
+++ docs/game-project/load.js
@@ -93,10 +93,3 @@ function drawLayerTiles(layer) {
 }
 
 function hexToRgb(hex) { return color(hex); }
-
-class Platform {
-   constructor(x, y, w, h) {
-      this.x = x; this.y = y; this.w = w; this.h = h;
-   }
-   display() { }
-}
