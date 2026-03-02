d612faaf107c7e8d8d802a1364fe1b861cd7499f
n-wind-ddd <luruomu8427506@163.com>
2026-03-02 12:28:56 +0000

    adjust logic of func of reset

--- docs/game-project/entity/Rest.js
+++ docs/game-project/entity/Rest.js
@@ -4,11 +4,8 @@ class Rest extends Entity {
    }
 
    onPlayerContact(player, gm) {
-      player.setPrompt('F');
-      if (keyIsDown(Keys.F)) {
-         player.hp = player.maxHp;
-         gm.saveCheckpoint(gm.levelIndex, this.x + this.w * 0.15, this.y - player.h);
-         gm.addParticles(this.cx(), this.cy());
-      }
+      player.hp = player.maxHp;
+      gm.saveCheckpoint(gm.levelIndex, this.x + this.w * 0.15, this.y - player.h);
+      gm.addParticles(this.cx(), this.cy());
    }
 }
